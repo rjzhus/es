c9f05ce49c9dd43727e539c33b51373cc5da2e30
zrj-coder <854530110@qq.com>
2021-06-12 16:28:03 +0800

    es init

---
+++ es-springdata/src/main/java/com/example/es/dao/ProductDao.java
@@ -0,0 +1,9 @@
+package com.example.es.dao;
+
+import com.example.es.entity.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductDao extends ElasticsearchRepository<Product,Long> {
+}
